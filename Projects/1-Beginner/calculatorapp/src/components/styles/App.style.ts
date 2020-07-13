6db771d1fae87ae230a543e66407c1a15707a51a
João Mello <joaomellogomes@gmail.com>
2020-07-12 22:31:09 -0300

    Added main layout scaffold

---
+++ Projects/1-Beginner/calculatorapp/src/components/styles/App.style.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native'
+
+export const theme = {
+	color: '',
+	backgroundColor: ''
+}
+
+export const Container = styled.View`
+	background-color: lightgrey;
+	flex: 1;
+`
+
+export const InputWrapper = styled.View`
+	flex: 0.3;
+	background-color: lightblue;
+`
+
+export const ButtonsWrapper = styled.View`
+	flex: 0.7;
+	background-color: lightgreen;
+`
